c5351abe0c73a381de36624fb99f388e799583c8
shashidhar <shashidhargr26@gmail.com>
2023-05-17 13:14:15 +0530

    Directory changes

---
+++ Arrays/BinaryAddition.java
@@ -0,0 +1,16 @@
+public class BinaryAddition {
+    public static void main(String[] args) {
+        String str1 = "00";
+        String str2 = "11";
+
+        String str = addBinary(str1, str2);
+
+        System.out.println(str);
+    }
+
+    static String addBinary(String str1, String str2) {
+        String str = "";
+
+        return str;
+    }
+}
